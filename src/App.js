c33cf1580e37f5973eefe5445b4564c3f155ad05
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-12-04 18:01:04 +0100

    Fix #1 by setting up project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
+/* globals window */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import autobind from 'react-autobind';
 
-class App extends Component {
+import { Wizard, StyleProvider } from 'losen';
+import data from './api/stort-bygge.json';
+import dataExport from './exports/data-export';
+import Intro from './pages/Intro';
+import store from './store';
+
+export default class App extends Component {
+  static propTypes = {
+    translations: PropTypes.object,
+  };
+
+  static defaultProps = {
+    translations: {},
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      intro: true,
+    };
+    autobind(this);
+  }
+
+  closeIntro() {
+    this.setState({ intro: false });
+    window.scrollTo(0, 0);
+  }
+
+  showIntro() {
+    this.setState({ intro: true });
+    window.scrollTo(0, 0);
+  }
+
   render() {
+    if (this.state.intro) {
+      return (
+        <Provider store={store}>
+          <StyleProvider>
+            <Intro close={this.closeIntro} />
+          </StyleProvider>
+        </Provider>
+      );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Wizard
+          wizard={data}
+          exports={{ dataExport }}
+          translations={this.props.translations}
+          showIntro={this.showIntro}
+        />
+      </Provider>
     );
   }
 }
-
-export default App;
