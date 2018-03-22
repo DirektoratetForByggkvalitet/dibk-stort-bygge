6bf3a61495bee3309772adb855c3c46861d8a01d
Tobias Goulden Schultz <tobias.g.schultz@gmail.com>
2018-03-22 13:34:44 +0100

    feat: present json on result page

--- src/exports/data-export.js
+++ src/exports/data-export.js
@@ -1,13 +1,9 @@
 import get from 'lodash.get';
 
 export default function exportData(state) {
-  const tomtearealByggeområde = get(state, 'propertyArea');
+  const tomtearealByggeomraade = get(state, 'propertyArea');
   const tomtearealSomTrekkesFra = get(state, 'nonSettlementArea');
-  const tomtearealBeregnet = tomtearealByggeområde - tomtearealSomTrekkesFra;
-
-  const arealBebyggelseSomSkalRives = get(state, 'arealBebyggelseSomSkalRives');
-  const arealBebyggelseNytt = get(state, 'newBuiltArea');
-  const parkeringsArealTerreng = arealBebyggelseSomSkalRives * arealBebyggelseNytt;
+  const tomtearealBeregnet = tomtearealByggeomraade - tomtearealSomTrekkesFra;
 
   // if area is calculated in percentage and not m2
   // there should be a more reliable way to do this?
@@ -15,29 +11,22 @@ export default function exportData(state) {
   const isPercentage = state.kommuneplanen.substring(0, 1) === 'p';
 
   return {
-    fraSluttbrukersystem: 'katteveiviser',
-    eiendomByggested: {
-      etasje: parseInt(get(state, 'living.floor'), 10),
-    },
-    ansvarsrett: {
-      noeGreier: 'her',
-    },
-    tomtearealByggeområde,
+    tomtearealByggeomraade,
     tomtearealSomTrekkesFra,
     tomtearealBeregnet,
 
-    arealBebyggelseEksisterende: (
-      get(state, 'builtResidence')
-      + get(state, 'builtOther')
-      + get(state, 'builtGarage')
-      + get(state, 'builtSmallBuilding')
-    ),
+    arealBebyggelseEksisterende:
+      get(state, 'builtResidence') +
+      get(state, 'builtOther') +
+      get(state, 'builtGarage') +
+      get(state, 'builtSmallBuilding'),
 
-    arealBebyggelseSomSkalRives,
-    arealBebyggelseNytt,
-    parkeringsArealTerreng,
+    arealBebyggelseSomSkalRives: get(state, 'arealBebyggelseSomSkalRives'),
+    arealBebyggelseNytt: get(state, 'newBuiltArea'),
+    parkeringsArealTerreng:
+      get(state, 'requiredParkingSpotsTerrain') * get(state, 'parkingPlaceArea'),
 
-    arealSumByggesak: get(state, 'resultGroup'),
-    beregnetGradAvUtnytting: get(state, 'sum2-planArea'),
+    arealSumByggesak: isPercentage ? get(state, 'resultGroup') : null,
+    beregnetGradAvUtnytting: get(state, 'resultGroup'),
   };
 }
