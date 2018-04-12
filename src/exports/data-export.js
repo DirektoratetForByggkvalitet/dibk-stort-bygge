a1bec8082bd23c9e0a7edf70ba5b56fec44880f4
Tobias Goulden Schultz <tobias.g.schultz@gmail.com>
2018-04-12 14:35:57 +0200

    feat: remove unnecessary questions from parking and others

--- src/exports/data-export.js
+++ src/exports/data-export.js
@@ -1,32 +1,35 @@
 import get from 'lodash.get';
 
 export default function exportData(state) {
-  const tomtearealByggeomraade = get(state, 'propertyArea');
-  const tomtearealSomTrekkesFra = get(state, 'nonSettlementArea');
+  const tomtearealByggeomraade = get(state, 'propertyArea') || 0;
+  const tomtearealSomTrekkesFra = get(state, 'nonSettlementArea') || 0;
   const tomtearealBeregnet = tomtearealByggeomraade - tomtearealSomTrekkesFra;
 
   // if area is calculated in percentage and not m2
   // there should be a more reliable way to do this?
   // eslint-disable-next-line no-unused-vars
   const isPercentage = state.kommuneplanen.substring(0, 1) === 'p';
+  const beregningsregelGradAvUtnytting = isPercentage ? `%${state.kommuneplanen.substring(1)}` : state.kommuneplanen;
+
   return {
     tomtearealByggeomraade,
     tomtearealSomTrekkesFra,
     tomtearealBeregnet,
 
     arealBebyggelseEksisterende:
-      get(state, 'builtResidence') +
-      get(state, 'builtOther') +
-      get(state, 'builtGarage') +
-      get(state, 'builtSmallBuilding'),
+      get(state, 'builtResidence') || 0 +
+      get(state, 'builtOther') || 0 +
+      get(state, 'builtGarage') || 0 +
+      get(state, 'builtSmallBuilding') || 0,
 
-    arealBebyggelseSomSkalRives: get(state, 'arealBebyggelseSomSkalRives'),
-    arealBebyggelseNytt: get(state, 'newBuiltArea'),
+    arealBebyggelseSomSkalRives: get(state, 'arealBebyggelseSomSkalRives') || 0,
+    arealBebyggelseNytt: get(state, 'newBuiltArea') || 0,
     parkeringsArealTerreng:
-      get(state, 'requiredParkingSpotsTerrain') * get(state, 'parkingPlaceArea'),
+      get(state, 'requiredParkingSpotsTerrain') || 0 * get(state, 'parkingPlaceArea') || 0,
 
-    arealSumByggesak: isPercentage ? get(state, 'resultGroup') : null,
-    beregnetGradAvUtnytting: get(state, 'resultGroup'),
-    beregningsregelGradAvUtnytting: state.kommuneplanen,
+    arealSumByggesak: isPercentage ? get(state, 'resultGroup') || 0 : null,
+    beregnetGradAvUtnytting: get(state, 'resultGroup') || 0,
+
+    beregningsregelGradAvUtnytting,
   };
 }
--- src/store.js
+++ src/store.js
@@ -3,12 +3,12 @@
 import { createStore, combineReducers, compose } from 'redux';
 import { state } from 'losen';
 import { persistStore, autoRehydrate } from 'redux-persist';
-
+import stortBygge from './api/stort-bygge.json';
 /**
  * Create the store with middleware
  */
 const store = createStore(
-  combineReducers({ [state.NAME]: state.reducer }),
+  combineReducers({ [state.NAME]: state.reducer(stortBygge) }),
   undefined,
   compose(autoRehydrate(), window.devToolsExtension ? window.devToolsExtension() : f => f),
 );
