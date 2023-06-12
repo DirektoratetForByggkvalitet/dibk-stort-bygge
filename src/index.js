d577f0fae9dc04c8a8e076a16f849ac0fdfcbcbc
Kristoffer Brabrand <kristoffer@brabrand.no>
2023-06-12 09:37:28 +0200

    Skip translations without a name

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ let translations = JSON.parse(root.getAttribute('data-bind') || '{}');
 translations = Object.keys(translations).reduce((res, id) => {
   const { title: heading, ...rest } = translations[id];
 
+  if (!heading) { return res; }
+
   return {
     ...res,
     [id]: {
